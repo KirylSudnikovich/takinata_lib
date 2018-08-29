3e93b6046743328bd3103cd20ea04903a4a85871
0311snitch <0311snitch@gmail.com>
2018-08-29 22:41:35 +0300

    [chore] little cheanup

--- venv/lib/python3.5/site-packages/takinata_lib-0.1-py3.5.egg/console/presentations/regular_task.py
+++
@@ -1,5 +0,0 @@
-class RegularTaskView:
-    @staticmethod
-    def create_format():
-        print("To create a task, enter the command as a task add 'username' 'password' 'project name' "
-              "'column name' 'task name' 'description' 'first date' 'second date' 'step' 'type_of_step' 'tags' 'priority'")
