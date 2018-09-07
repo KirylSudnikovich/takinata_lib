c7527450558b64c5ced43f7c31bb905cccd33861
Kirill Sudnikovich <0311snitch@gmail.com>
2018-09-07 23:30:00 +0300

    [chore]

--- libsetup/build/lib/lib/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""A package that contains a description of all the entities that are used in the library
-
-__________
-
-Description of internal files
-
-models - description for all exists models
-
-"""
--- libsetup/lib/models/base_model.py
+++
@@ -1,8 +0,0 @@
-from peewee import *
-import lib.conf as config
-
-db = SqliteDatabase(config.get_path_to_db())
-
-class BaseModel(Model):
-    class Meta:
-        database = db
